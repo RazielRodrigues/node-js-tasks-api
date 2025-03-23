093391fb236242934397fa14cb994f2bb1a4e20c
Raziel Rodrigues <razielx3@live.com>
2025-03-23 15:25:25 +0000

    feat: starting login with JWT token

--- app/app.js
+++ app/app.js
@@ -12,12 +12,27 @@ app.listen(process.env.PORT, () => {
 // Sync da base de dados (adicionar migrations?)
 database.connection.sync().then(() => { console.log('Database Connected...'); }).catch((err) => { console.log(err); })
 
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    jwt.verify(token, 'secret', (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
 // Registrando controllers e rotas
 const homeController = require('./src/controller/homeController')
 app.use('/', homeController)
 
 const taskController = require('./src/controller/taskController')
-app.use('/task', taskController)
+app.use('/task', verifyToken, taskController)
 
 const userController = require('./src/controller/userController')
 app.use('/user', userController)
--- app/src/controller/userController.js
+++ app/src/controller/userController.js
@@ -1,51 +1,97 @@
 const express = require('express')
 const router = express.Router()
 const database = require('../model/index')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { userModel } = database;
+
+
+const userRole = {
+    1: 'manager',
+    2: 'techinician'
+}
+
+
+// Route to authenticate and log in a user
+router.post('/login', async (req, res) => {
+    try {
+ 
+        const user = await userModel.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ email: user.email }, 'secret');
+        res.status(200).json({ token });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 
 // create
 router.post('/', async (req, res) => {
+    const user = await userModel.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+    if (user) {
+        res.status(400).json({ error: 'Email already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
     const data = await userModel.create({
         email: req.body.email,
-        password: req.body.password,
+        password: hashedPassword,
         role: req.body.role,
     })
 
-    res.json({ status: 201, data: data })
-    res.status(201)
+    res.status(201).json({ data: data })
 })
 
 // read all
 router.get('/', async (req, res) => {
+    try {
+        const data = await userModel.findAll();
+        if (!data) {
+            res.status(204).json({ data: [] })
+        }
 
-    const data = await userModel.findAll();
-    if (!data) {
-        res.json({
-            status: 204,
-            data: []
-        })
-        res.status(204)
+        res.status(200).json({ data: data })
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
     }
-
-    res.json({ status: 200, data: data })
-    res.status(200)
 })
 
 // read one
 router.get('/:id', async (req, res) => {
 
-    const data = await userModel.findOne({ where: { id: req.params.id } });
-    if (!data) {
-        res.json({
-            status: 404,
-            data: `User not found for id #${req.params.id}`
-        })
-        res.status(404)
-    }
 
-    res.json({ status: 200, data: data })
-    res.status(200)
+    try {
+        const data = await userModel.findOne({ where: { id: req.params.id } });
+        if (!data) {
+            res.json({
+                status: 404,
+                data: `User not found for id #${req.params.id}`
+            })
+            res.status(404)
+        }
+ 
+
+        res.status(200).json({ data: data })
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
 })
 
 // update

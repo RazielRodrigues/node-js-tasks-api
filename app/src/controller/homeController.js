abec7105e57cb73ef9abac1c9d16b83ff14fdba1
Raziel Rodrigues <razielx3@live.com>
2025-03-24 17:58:02 +0000

    feat: home controller design

--- app/src/controller/homeController.js
+++ app/src/controller/homeController.js
@@ -3,7 +3,13 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
     try {
-        res.status(200).json({ data: 'Welcome to the sword health tasks API' })
+        res.status(200).json({
+            data: {
+                message: 'Welcome to the sword health tasks API',
+                description: 'Get your token and start managing your tasks',
+                repository: 'https://github.com/RazielRodrigues/sword-health-tasks-api',
+                doc: 'https://github.com/RazielRodrigues/sword-health-tasks-api/blob/main/sword_health_tasks_api_postman_collection.json',
+        } })
     } catch (error) {
         res.status(500).json({ data: 'Internal server error' });
     }

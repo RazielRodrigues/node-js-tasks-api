fe06aeae4e9f02643c554ba7367a3b2e16bd73c2
Raziel Rodrigues <razielx3@live.com>
2025-03-24 17:57:53 +0000

    feat: new adjusts in the app for refactoring and test environment improvemente with sinon

--- app/app.js
+++ app/app.js
@@ -1,18 +1,15 @@
 const express = require('express')
 const cors = require('cors')
-const jwt = require('jsonwebtoken');
 const app = express()
 const database = require('./src/model/index')
 const amqp = require('amqplib');
-
 app.use(cors({ origin: process.env.CORS_ORIGIN + process.env.PORT }))
 app.use(express.json())
 app.listen(process.env.PORT, () => {
 
-    // Message broker com rabbimq https://github.com/amqp-node/amqplib/tree/main/examples/tutorials
+    // inciei o rabbitMQ
     (async () => {
         try {
-
             const exchange = 'logs';
             const connection = await amqp.connect(process.env.MESSAGE_BROKER);
             const channel = await connection.createChannel();
@@ -32,36 +29,30 @@ app.listen(process.env.PORT, () => {
                 else console.warn(' [x] Consumer cancelled');
             }, { noAck: true });
 
-            console.log(' [*] Waiting for logs. To exit press CTRL+C');
+            console.log('\n[*] Waiting for API logs...');
         } catch (err) {
+            healthcheck = 504;
             console.warn(err);
         }
     })();
 
-    console.log('Sword health tasks api ready: ' + process.env.PORT)
-})
-
-// Token middleware
-const verifyToken = (req, res, next) => {
-    let token = req.headers['authorization'];
-    if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    token = token.replace('Bearer ', '');
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-
-        if (err) {
-            return res.status(401).json({ error: 'Unauthorized' });
+    // Iniciando a base de dados
+    (async () => {
+        try {
+            await database.connection.sync()
+            console.log('\nDatabase Connected...')
+        } catch (err) {
+            healthcheck = 504;
+            console.warn(err);
         }
+    })();
 
-        req.user = decoded;
-        next();
-    });
-};
+    // Message
+    (async () => {
+        console.log('\nSword health tasks API ready: ' + process.env.DOMAIN + ':' + process.env.PORT)
+    })();
 
-// Sync da base de dados (adicionar migrations?)
-database.connection.sync().then(() => { console.log('Database Connected...'); }).catch((err) => { console.log(err); })
+})
 
 // Registrando controllers e rotas
 const homeController = require('./src/controller/homeController')
@@ -71,7 +62,7 @@ const loginController = require('./src/controller/loginController')
 app.use('/login', loginController)
 
 const taskController = require('./src/controller/taskController')
-app.use('/task', verifyToken, taskController)
+app.use('/task', taskController)
 
 const userController = require('./src/controller/userController')
-app.use('/user', verifyToken, userController)
+app.use('/user', userController)

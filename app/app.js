599dd36172098a6ca50964fb02b653279b50282a
Raziel Rodrigues <razielx3@live.com>
2025-03-24 21:04:19 +0000

    refactor: leveraging app file removing the database call and rabbitmq starting from there

--- app/app.js
+++ app/app.js
@@ -1,60 +1,35 @@
 const express = require('express')
 const cors = require('cors')
-const app = express()
+const { startLog } = require('./src/service/logService')
 const database = require('./src/model/index')
-const amqp = require('amqplib');
+const app = express()
+
 app.use(cors({ origin: process.env.CORS_ORIGIN + process.env.PORT }))
 app.use(express.json())
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
 
-    // inciei o rabbitMQ
-    (async () => {
-        try {
-            const exchange = 'logs';
-            const connection = await amqp.connect(process.env.MESSAGE_BROKER);
-            const channel = await connection.createChannel();
+    // Healthcheck
+    console.log('\nSword health tasks API ready: ' + process.env.BASE_URL + ':' + process.env.PORT)
 
-            process.once('SIGINT', async () => {
-                await channel.close();
-                await connection.close();
-            });
+    // Iniciar a base de dados
+    try {
+        await database.connection.sync()
+        console.log('\nDatabase Connected...')
+    } catch (err) {
+        console.warn(err);
+    }
 
-            await channel.assertExchange(exchange, 'fanout', { durable: false });
-
-            const { queue } = await channel.assertQueue('', { exclusive: true });
-            await channel.bindQueue(queue, exchange, '')
-
-            await channel.consume(queue, (message) => {
-                if (message) console.log(" [x] '%s'", message.content.toString());
-                else console.warn(' [x] Consumer cancelled');
-            }, { noAck: true });
-
-            console.log('\n[*] Waiting for API logs...');
-        } catch (err) {
-            healthcheck = 504;
-            console.warn(err);
-        }
-    })();
-
-    // Iniciando a base de dados
-    (async () => {
-        try {
-            await database.connection.sync()
-            console.log('\nDatabase Connected...')
-        } catch (err) {
-            healthcheck = 504;
-            console.warn(err);
-        }
-    })();
-
-    // Message
-    (async () => {
-        console.log('\nSword health tasks API ready: ' + process.env.DOMAIN + ':' + process.env.PORT)
-    })();
+    // Iniciar o message broker
+    try {
+        await startLog();
+        console.log('\nLogs Connected...');
+    } catch (err) {
+        console.warn(err);
+    }
 
 })
 
-// Registrando controllers e rotas
+// Registrando controllers
 const homeController = require('./src/controller/homeController')
 app.use('/', homeController)
 
@@ -66,3 +41,5 @@ app.use('/task', taskController)
 
 const userController = require('./src/controller/userController')
 app.use('/user', userController)
+
+module.exports = app

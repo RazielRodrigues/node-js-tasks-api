9242e4c872b5d036bc2799e8f9c50bb296f55deb
Raziel Rodrigues <razielx3@live.com>
2025-03-24 21:06:10 +0000

    refactor: task controller impovements and test

--- app/src/controller/taskController.js
+++ app/src/controller/taskController.js
@@ -1,52 +1,50 @@
 const express = require('express')
 const router = express.Router()
 const database = require('../model/index');
+const { ROLE_MANAGER, ROLE_TECHNICIAN } = require('../model/enum');
 const { taskModel, userModel } = database;
-const amqp = require('amqplib');
+const verifyToken = require('../service/verifyTokenService')
+const {sendLog} = require('../service/logService')
 
-// todo: transfer to other file
-const ROLE_MANAGER = 1
-const ROLE_TECHNICIAN = 2
-
-router.post('/', async (req, res) => {
+router.post('/', verifyToken, async (req, res) => {
     try {
         const user = await userModel.findOne({ where: { email: req.user.email } });
 
         if (user.role !== ROLE_TECHNICIAN) {
-            return res.status(401).json({ error: 'Unauthorized' });
+            return res.status(401).json({ error: 'Unauthorized: only technicians can create tasks' });
         }
 
         const data = await taskModel.create({
-            summary: req.body.summary, // todo: add encryption
+            summary: req.body.summary,
             completed_at: null,
             userId: user.id
         })
 
-        res.status(201).json({ data: data })
+        return res.status(201).json({ data: `Your task has been created with ID #${data.id}` })
     } catch (error) {
-        res.status(500).json({ data: 'Internal server error' });
+        return res.status(500).json({ data: 'Internal server error' });
     }
 })
 
-router.get('/', async (req, res) => {
+router.get('/', verifyToken, async (req, res) => {
     try {
         const user = await userModel.findOne({ where: { email: req.user.email } });
 
         const data = user.role === ROLE_MANAGER
             ? await taskModel.findAll()
-            : await taskModel.findAll({ where: { userId: user.id } });
+            : await taskModel.findOne({ where: { userId: user.id } });
 
         if (!data) {
-            res.status(204).json({ data: data })
+            return res.status(204).json({ data: data })
         }
 
-        res.status(200).json({ data: data })
+        return res.status(200).json({ data: data })
     } catch (error) {
-        res.status(500).json({ data: 'Internal server error' });
+        return res.status(500).json({ data: 'Internal server error' });
     }
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', verifyToken, async (req, res) => {
     try {
         const user = await userModel.findOne({ where: { email: req.user.email } });
 
@@ -54,100 +52,97 @@ router.get('/:id', async (req, res) => {
             ? await taskModel.findOne({ where: { id: req.params.id } })
             : await taskModel.findOne({ where: { id: req.params.id, userId: user.id } });
 
+        if (!data && user.role !== ROLE_MANAGER) {
+            return res.status(401).json({ error: 'Unauthorized: you are not allowed to see this task' });
+        }
+        
         if (!data) {
-            res.status(404).json({ data: `Task not found for ID #${req.params.id}` })
+            return res.status(404).json({ data: `Task not found for ID #${req.params.id}` })
         }
 
-        res.status(200).json({ data: data })
+        return res.status(200).json({ data: data })
     } catch (error) {
-        res.status(500).json({ data: 'Internal server error' });
+        return res.status(500).json({ data: 'Internal server error' });
     }
 })
 
-router.put('/:id', async (req, res) => {
-    try {
-        const user = await userModel.findOne({ where: { email: req.user.email } });
-
-        const task = user.role === ROLE_MANAGER
-            ? await taskModel.findOne({ where: { id: req.params.id } })
-            : await taskModel.findOne({ where: { id: req.params.id, userId: user.id } });
-
-        if (!task) {
-            res.status(404).json({ data: `Task not found for ID #${req.params.id}` })
-        }
-
-        const data = await taskModel.update({
-            summary: req.body.summary, // todo: add encryption
-            completed_at: req.body.completed ? new Date() : null,
-        }, { where: { id: req.params.id, } })
-
-        res.status(200).json({ data: data })
-    } catch (error) {
-        res.status(500).json({ data: 'Internal server error' });
-    }
-})
-
-router.delete('/:id', async (req, res) => {
-    try {
-        const user = await userModel.findOne({ where: { email: req.user.email } });
-
-        const task = user.role === ROLE_MANAGER
-            ? await taskModel.findOne({ where: { id: req.params.id } })
-            : await taskModel.findOne({ where: { id: req.params.id, userId: user.id } });
-
-        if (!task) {
-            res.status(404).json({ data: `Task not found for ID #${req.params.id}` })
-        }
-
-        const data = await taskModel.destroy({ where: { id: task.id } })
-
-        res.status(200).json({ data: data })
-    } catch (error) {
-        res.status(500).json({ data: 'Internal server error' });
-    }
-})
-
-router.put('/completed/:id', async (req, res) => {
+router.put('/:id', verifyToken, async (req, res) => {
     try {
         const user = await userModel.findOne({ where: { email: req.user.email } });
 
         if (user.role !== ROLE_TECHNICIAN) {
-            return res.status(401).json({ error: 'Unauthorized' });
+            return res.status(401).json({ error: 'Unauthorized: only technicians can update tasks' });
+        }
+
+        const task = user.role === ROLE_MANAGER
+            ? await taskModel.findOne({ where: { id: req.params.id } })
+            : await taskModel.findOne({ where: { id: req.params.id, userId: user.id } });
+
+        if (!task) {
+            return res.status(404).json({ data: `Task not found for ID #${req.params.id}` })
+        }
+
+        const data = await taskModel.update({
+            summary: req.body.summary,
+            completed_at: req.body.completed ? new Date() : null,
+        }, { where: { id: req.params.id, } })
+
+        return res.status(200).json({ data: `Your task has been updated with ID #${req.params.id}` })
+    } catch (error) {
+        return res.status(500).json({ data: 'Internal server error' });
+    }
+})
+
+router.delete('/:id', verifyToken, async (req, res) => {
+    try {
+        const user = await userModel.findOne({ where: { email: req.user.email } });
+
+        if (user.role !== ROLE_MANAGER) {
+            return res.status(401).json({ error: 'Unauthorized: only managers can delete tasks' });
+        }
+
+        const task = user.role === ROLE_MANAGER
+            ? await taskModel.findOne({ where: { id: req.params.id } })
+            : await taskModel.findOne({ where: { id: req.params.id, userId: user.id } });
+
+        if (!task) {
+            return res.status(404).json({ data: `Task not found for ID #${req.params.id}` })
+        }
+
+        await taskModel.destroy({ where: { id: task.id } })
+
+        return res.status(200).json({ data: `Your task has been deleted with the email: ${task.id}` })
+    } catch (error) {
+        return res.status(500).json({ data: 'Internal server error' });
+    }
+})
+
+router.patch('/completed/:id', verifyToken, async (req, res) => {
+    try {
+        const user = await userModel.findOne({ where: { email: req.user.email } });
+
+        if (user.role !== ROLE_TECHNICIAN) {
+            return res.status(401).json({ error: 'Unauthorized: only technicians can update tasks' });
         }
 
         const task = await taskModel.findOne({ where: { id: req.params.id, userId: user.id } })
 
         if (!task) {
-            res.status(404).json({ data: `Task not found for ID #${req.params.id}` })
+            return res.status(404).json({ data: `Task not found for ID #${req.params.id}` })
         }
 
+        const time = new Date();
         const data = await taskModel.update({
-            completed_at: new Date(),
+            completed_at: time,
         }, { where: { id: req.params.id, } })
 
-        const exchange = 'logs';
-        const text = `Task with ID #${req.params.id} updated to complete`;
-        (async () => {
-            let connection;
-            try {
-                connection = await amqp.connect('amqp://localhost');
-                const channel = await connection.createChannel();
-                await channel.assertExchange(exchange, 'fanout', { durable: false });
-                channel.publish(exchange, '', Buffer.from(text));
-                console.log(" [x] Sent '%s'", text);
-                await channel.close();
-            }
-            catch (err) {
-                console.warn(err);
-            }
-            finally {
-                if (connection) await connection.close();
-            };
-        })();  
+        const text = `Task with ID #${req.params.id} updated to complete at: ` + time.toUTCString();
+        await sendLog(text)
 
-        res.status(200).json({ data: data })
+        return res.status(200).json({ data: data })
     } catch (error) {
-        res.status(500).json({ data: 'Internal server error' });
+        console.log(error)
+        return res.status(500).json({ data: 'Internal server error' });
     }
 })
 
